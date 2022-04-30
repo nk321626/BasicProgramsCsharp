b2e25d00854c4a667026c4201d32279a36c6da7f
nk321626 <nk321626@gmail.com>
2022-04-30 15:16:36 +0530

    Leap Year Program

---
+++ BasicProgramsCsharp/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace  LeapYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year;
+            Console.WriteLine("Enter the Year");
+            year = Convert.ToInt32(Console.ReadLine());
+            if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+                Console.WriteLine("Year is Leap Year");
+            else
+                Console.WriteLine("Year is not a Leap Year");
+
+            Console.ReadKey();
+        }
+
+    }
+}
