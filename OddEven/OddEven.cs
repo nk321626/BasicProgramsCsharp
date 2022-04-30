ec9fd983587cc344059091744938fc74d5fc5418
nk321626 <nk321626@gmail.com>
2022-04-30 22:46:25 +0530

    check Odd or Even Output

---
+++ OddEven/OddEven.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddEven
+{
+    internal class OddEven
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Number to check");
+            int num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Number is Even");
+            }
+            else
+                Console.WriteLine("Number is Odd");
+
+        }
+    }
+}
