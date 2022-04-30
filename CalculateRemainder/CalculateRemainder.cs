c3578ef394e1de7956e9107d04c39759e6d1b3c7
nk321626 <nk321626@gmail.com>
2022-04-30 22:02:35 +0530

    Calculate remainder

---
+++ CalculateRemainder/CalculateRemainder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateRemainder
+{
+    internal class CalculateRemainder
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the dividend");
+            int dividend = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("enter the divisor");
+            int divisor = int.Parse(Console.ReadLine());
+
+            int remainder = dividend % divisor;
+            int quotient = dividend / divisor;
+
+            Console.WriteLine("Quotient is " + quotient);
+            Console.WriteLine("remainder is " + remainder);
+
+            //Console.WriteLine("Hello World"!);
+        }
+    }
+}
