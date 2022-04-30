cb4005ec720690d875e34b38878ea0115596300d
nk321626 <nk321626@gmail.com>
2022-04-30 16:14:28 +0530

    Check Power of Two

---
+++ PowerofTwo/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerofTwo
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the values of Power of Two");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(" table is ");
+            for (int i = 0; i < n; i++)
+            {
+                double p = Math.Pow(2, i);
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
