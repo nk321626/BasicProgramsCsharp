9bb10b9d9e700148b2d02dfcdb5b86d190b06404
nk321626 <nk321626@gmail.com>
2022-04-30 16:33:47 +0530

    Added Harmonic Program

---
+++ HarmonicNumber/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarmonicNumber
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the value N");
+            int n = int.Parse(Console.ReadLine());
+            float h = 0;
+            for (int i = 0; i < n; i++)
+            {
+                h += (float)1 / i;
+            }
+            Console.WriteLine(" Nth value of harmoic number is " + "" + h);
+        }
+    }
+}
