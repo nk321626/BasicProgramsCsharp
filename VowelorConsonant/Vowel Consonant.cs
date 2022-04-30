58d1ebf95db8fc48aad69d059c92ed1508c4a631
nk321626 <nk321626@gmail.com>
2022-04-30 23:11:23 +0530

    check vowel Consonent

---
+++ VowelorConsonant/Vowel Consonant.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VowelorConsonant
+{
+    internal class Vowel_Consonant
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Alphabet to check");
+            char alpha = char.Parse(Console.ReadLine());
+            switch (alpha)
+            {
+                case 'a':
+                    Console.WriteLine("Alphabet is vowel1");
+                    break;
+                case 'A':
+                    Console.WriteLine("Alphabet is vowel1");
+                    break ;
+                case 'e':
+                    Console.WriteLine("Alphabet is vowel1");
+                    break;
+                case'E':
+                    Console.WriteLine("Alphabet is vowel1");
+                    break;
+                case 'i': 
+                    Console.WriteLine("Alphabet is vowel1");
+                    break;
+                case 'I':
+                    Console.WriteLine("Alphabet is vowel1");
+                    break;
+                case 'o':
+                    Console.WriteLine("Alphabet is vowel1");
+                    break;
+                case 'O':
+                    Console.WriteLine("Alphabet is vowel1");
+                    break;
+                case 'u':
+                    Console.WriteLine("Alphabet is vowel1");
+                    break;
+                case 'U':
+                    Console.WriteLine("Alphabet is vowel1");
+                    break;
+                default:
+                    Console.WriteLine("Alphabet is Consonent");
+                    break;
+
+             }
+        }
+    }
+}
