e10295fde05e1ca9ce18cc39364b22729ead46bc
nk321626 <nk321626@gmail.com>
2022-05-01 17:42:35 +0530

    No of times random Coupon no generated

---
+++ CouponNumber/CouponNum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CouponNumber
+{
+    internal class CouponNum
+    {
+        static void Main(string[] args)
+        {
+            int ranNum = 0;
+            int flag = 0;
+            Console.WriteLine("enter the coupon num");
+            int cnum = int.Parse(Console.ReadLine());
+            // Random ran = new random();
+            while (ranNum != cnum)
+            {
+                Random ran = new Random();
+                ranNum = ran.Next(0, 100);
+                Console.WriteLine(ranNum);
+                flag++;
+            }
+            Console.WriteLine("no of times random no generated is " + flag);
+
+        }
+    }
+}
