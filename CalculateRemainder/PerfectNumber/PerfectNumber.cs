e9c62b7abd3e433ca0ed8c322118f3938aed5ab7
nk321626 <nk321626@gmail.com>
2022-05-01 14:17:30 +0530

    checked Perfect Number

---
+++ CalculateRemainder/PerfectNumber/PerfectNumber.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerfectNumber
+{
+    internal class PerfectNumber
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            Console.WriteLine("Enter the Number to check");
+            int num = int.Parse(Console.ReadLine());
+            int m = num / 2;
+            for (int i = 1; i <= m; i++)
+            {
+                int rem = num % i;
+                if (rem == 0)
+                {
+                    Console.WriteLine(i);
+                    sum = sum + i;
+                }
+                //else
+                //break;
+
+            }
+            if (sum == num)
+            {
+                Console.WriteLine("Number is Perfect Number");
+            }
+            else
+                Console.WriteLine("Number is Not Perfect NumberS");
+        }
+    }
+}
